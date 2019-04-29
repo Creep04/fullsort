876d756d48f3229d1719cb0e342472eb4e6c5311
Theo Djian <creep@Creep-Macbook.local>
2019-04-29 19:51:13 +0200

    Added comparators.go

---
+++ comparators.go
@@ -0,0 +1,17 @@
+package fullsort
+
+func stringComparator(a, b interface{}) bool {
+	return false
+}
+
+func intComparator(a, b interface{}) bool {
+	return false
+}
+
+func floatComparator(a, b interface{}) bool {
+	return false
+}
+
+func complexComparator(a, b interface{}) bool {
+	return false
+}
