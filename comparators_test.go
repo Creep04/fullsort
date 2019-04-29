4a1127bf7e9646a653f41d32deec93a647903df0
Theo Djian <creep@Creep-Macbook.local>
2019-04-29 20:15:43 +0200

    Added comparators_test.go and created some tests

---
+++ comparators_test.go
@@ -0,0 +1,91 @@
+package fullsort
+
+import "testing"
+
+func TestStringComparator(t *testing.T) {
+	a, b := "aaaa", "ba"
+	if stringComparator(a, b) {
+		t.Error("Compare \"aaaa\" and \"ba\": expected false")
+	}
+	if !stringComparator(b, a) {
+		t.Error("Compare \"ba\" and \"aaaa\": expected true")
+	}
+	if stringComparator("", "") {
+		t.Error("Compare empty strings: expected false")
+	}
+	if stringComparator("abcdef", "abcdef") {
+		t.Error("Compare \"abcdef\" and \"abcdef\": expected false")
+	}
+	if !stringComparator("ptdr", "") {
+		t.Error("Compare \"ptdr\" and empty string: expected true")
+	}
+	if stringComparator("hell", "hello") {
+		t.Error("Compare \"hell\" and \"hello\": expected false")
+	}
+}
+
+func TestFloatComparator32(t *testing.T) {
+	var a, b float32
+
+	a, b = 4.231, 42.5
+	if floatComparator(a, b) {
+		t.Error("Compare 4.231 and 42.5: expected false")
+	}
+	if !floatComparator(b, a) {
+		t.Error("Compare 42.5 and 4.231: expected true")
+	}
+	a, b = 0, 42
+	if floatComparator(a, b) {
+		t.Error("Compare 0 and 42: expected false")
+	}
+	if !floatComparator(b, a) {
+		t.Error("Compare 42 and 0: expected true")
+	}
+	a, b = -64, 12
+	if floatComparator(a, b) {
+		t.Error("Compare -64 and 12: expected false")
+	}
+	if !floatComparator(b, a) {
+		t.Error("Compare 12 and -64: expected true")
+	}
+
+	if a, b = 0, 0; floatComparator(a, b) {
+		t.Error("Compare 0 and 0: expected false")
+	}
+	if a, b = -35, -35; floatComparator(a, b) {
+		t.Error("Compare -35 and -35: expected false")
+	}
+}
+
+func TestFloatComparator64(t *testing.T) {
+	var a, b float64
+	
+	a, b = 4.231, 42.5
+	if floatComparator(a, b) {
+		t.Error("Compare 4.231 and 42.5: expected false")
+	}
+	if !floatComparator(b, a) {
+		t.Error("Compare 42.5 and 4.231: expected true")
+	}
+	a, b = 0, 42
+	if floatComparator(a, b) {
+		t.Error("Compare 0 and 42: expected false")
+	}
+	if !floatComparator(b, a) {
+		t.Error("Compare 42 and 0: expected true")
+	}
+	a, b = -64, 12
+	if floatComparator(a, b) {
+		t.Error("Compare -64 and 12: expected false")
+	}
+	if !floatComparator(b, a) {
+		t.Error("Compare 12 and -64: expected true")
+	}
+
+	if a, b = 0, 0; floatComparator(a, b) {
+		t.Error("Compare 0 and 0: expected false")
+	}
+	if a, b = -35, -35; floatComparator(a, b) {
+		t.Error("Compare -35 and -35: expected false")
+	}
+}
